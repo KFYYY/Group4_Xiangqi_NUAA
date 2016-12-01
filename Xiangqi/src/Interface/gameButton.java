5b1fee26481e22bcff2e613a7fb01c1f01e6748b
Chaojie Lv <15651651997@163.com>
2016-12-01 13:24:48 +0800

    在gameButton中，做一个类似于这样的按钮，包含starting，restarting，exiting，undoing a move这四个，只需要实现排版，不需要实现按钮的功能。

---
+++ Xiangqi/src/Interface/gameButton.java
@@ -0,0 +1,10 @@
+package Interface;
+
+import javax.swing.JPanel;
+
+//包含starting，restarting，exiting，undoing a move这四个，只需要实现排版，不需要实现按钮的功能
+public class gameButton extends JPanel{
+	gameButton(){
+		
+	}
+}
