eb4852d51123c6dd6f0cfb6395fd5c0170e91f97
Chaojie Lv <15651651997@163.com>
2016-12-04 23:04:45 +0800

    请实现isCorrect，GameOver

---
+++ Xiangqi/bin/rule/rule.java
@@ -0,0 +1,35 @@
+package rule;
+
+public class rule {
+	//b : black ; r : red;
+	//a : shi ; b : xiang ; c : pao ; k : jiang ; n : ma ; p : zu ; r : che ;
+	//oo : blank ;
+	private final int oo = 0;//空
+	private final int ba = 1;//黑 士
+	private final int bb = 2;
+	private final int bc = 3;
+	private final int bk = 4;
+	private final int bn = 5;
+	private final int bp = 6;
+	private final int br = 7;
+	
+	private final int ra = 8;
+	private final int rb = 9;
+	private final int rc = 10;
+	private final int rk = 11;
+	private final int rn = 12;
+	private final int rp = 13;
+	private final int rr = 14;
+	//board 中 各棋子的数值如上，判断(originX,originY)->(toX,toY)是否合法，保证起点有棋子
+	//board 9*10，左上为(0,0),右下为(8,9)
+	public static boolean isCorrect(int[][] board,int originX,int originY,int toX,int toY){
+		//填写
+		return false;
+	}
+	//如果有一方的将或帅没了，则游戏结束，1表示红方胜，2表示黑方胜；
+	//游戏未结束返回0
+	public static int GameOver(int[][] board){
+		//填写
+		return 0;
+	}
+}
